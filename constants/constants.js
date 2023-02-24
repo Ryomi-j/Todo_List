0efbc662b44aa45a5fe7cf9f70f93e175d65a7be
Ryomi-j <ryomi.j78@gmail.com>
2023-02-24 19:44:02 +0900

    change a constant name

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
 import { getDOM } from '../utils/dom.js';
 
 export const API_URL = 'http://localhost:3000/lists';
-export const $todos = getDOM('todos');
+export const LIST_CONTAINER = getDOM('.todo_wrap');
