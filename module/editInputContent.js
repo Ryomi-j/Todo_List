1eb51a1d80dcd929f8d3eb02705a7981751a545b
Ryomi-j <ryomi.j78@gmail.com>
2023-02-24 22:07:49 +0900

    edit editInputContent value

--- module/editInputContent.js
+++ module/editInputContent.js
@@ -1,14 +1,14 @@
 export const editInputContent = (e) => {
-    const $item = e.target.closest('.item')
-    const $label = $item.querySelector('.content')
-    const $editInput =  $item.querySelector('.todoInput')
-    const value = $editInput.value // 기존값
+  const $item = e.target.closest('.item');
+  const $label = $item.querySelector('.content');
+  const $editInput = $item.querySelector('.todoInput');
+  const value = $editInput.value; // 기존값
 
-    if(e.target.className === 'content') {
-        $label.style.display = 'none'
-        $editInput.style.display = 'block'
-        $editInput.focus()
-        $editInput.value = ""
-        $editInput.value = $label.innerText
-    }
-}
+  if (e.target.className === 'content') {
+    $label.style.display = 'none';
+    $editInput.style.display = 'block';
+    $editInput.focus();
+    $editInput.value = '';
+    $editInput.value = value;
+  }
+};
