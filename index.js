ded8cc77b408f0e5119dfec860c29a22e3f6e044
Ryomi-j <ryomi.j78@gmail.com>
2023-02-24 20:21:50 +0900

    make a function to add todo list item

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { getList } from "./module/fetch.js";
-import { getDOM } from "./utils/dom.js";
+import { getList } from './module/fetch.js';
+import { getDOM } from './utils/dom.js';
+import {addListItem} from './module/addListItem.js';
 
-const $form = getDOM('form')
+const $form = getDOM('form');
 
 const init = () => {
   window.addEventListener('DOMContentLoaded', () => {
     getList();
   });
-
-
+  $form.addEventListener('submit', addListItem);
 };
 
-init()
+init();
