db5590dd6aef76e20c3a5a3db39a0b2322fd659d
Ryomi-j <ryomi.j78@gmail.com>
2023-02-26 14:24:26 +0900

    remove annotation

--- module/toggleListItem.js
+++ module/toggleListItem.js
@@ -1,7 +1,6 @@
 import { API_URL } from '../constants/constants.js';
 import { getList } from './getList.js';
 
-// 체크박스 상태에 따라 completed의 상태를 T/F로 바꿔주는 함수
 export const toggleListItem = (e) => {
   if (e.target.className !== 'isDone') return;
   const $item = e.target.closest('.item');
