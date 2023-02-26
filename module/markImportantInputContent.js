99782071740b81f7da85cabcfcdf20f2958f1133
Ryomi-j <ryomi.j78@gmail.com>
2023-02-26 15:33:55 +0900

    trim code

--- module/markImportantInputContent.js
+++ module/markImportantInputContent.js
@@ -2,16 +2,10 @@ import { API_URL } from '../constants/constants.js';
 import { getList } from './getList.js';
 
 export const markImportantInputContent = (e) => {
-  let target;
-  if (e.target.tagName === 'BUTTON') {
-    target = e.target;
-  } else if (e.target.parentElement.tagName === 'BUTTON') {
-    target = e.target.parentNode;
-  }
-  if (target.classList === undefined || !target.classList.contains('importantBtn')) return;
-  const $item = target.closest('.item');
+  if (e.target.classList === undefined || !e.target.classList.contains('importantBtn')) return;
+  const $item = e.target.closest('.item');
   const id = $item.dataset.id;
-  const important = !target.classList.contains('active');
+  const important = !e.target.classList.contains('active');
   fetch(`${API_URL}/${id}`, {
     method: 'PATCH',
     headers: { 'Content-type': 'application/json' },
