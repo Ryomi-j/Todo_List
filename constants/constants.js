fe7d7cec1098e9eb53d814ff3f020921d523e93e
Kim Eunjeong <ryomi.j78@gmail.com>
2023-08-06 01:13:26 +0900

    add TITLE_API_UTL and update constants name

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,5 @@
-import { getDOM } from '../utils/dom.js';
+import { getDOM } from "../utils/dom.js";
 
-export const API_URL = 'http://localhost:3000/lists';
-export const LIST_CONTAINER = getDOM('.todo_wrap'); // <section class="todo_wrap">
+export const LIST_API_URL = "http://localhost:3000/lists";
+export const TITLE_API_URL = "http://localhost:3000/title";
+export const LIST_CONTAINER = getDOM(".todo_wrap"); // <section class="todo_wrap">
