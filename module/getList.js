512f42b9c62654280df8d6de6cf8fc08d2bdd69e
Kim Eunjeong <ryomi.j78@gmail.com>
2023-08-05 23:16:01 +0900

    make getListLength function to get list length

--- module/getList.js
+++ module/getList.js
@@ -1,10 +1,16 @@
-import { API_URL } from '../constants/constants.js';
-import { currentPage, limit } from './pagination.js';
-import { renderAllList } from './renderAllList.js';
+import { API_URL } from "../constants/constants.js";
+import { currentPage, limit } from "./pagination.js";
+import { renderAllList } from "./renderAllList.js";
 
 export const getList = () => {
   fetch(`${API_URL}?_page=${currentPage}&_limit=${limit}`)
     .then((response) => response.json())
     .then((item) => renderAllList(item))
     .catch((err) => console.log(err.message));
+};
+
+export const getListLength = async () => {
+  const res = await fetch(`${API_URL}`);
+  const data = await res.json();
+  return data.length;
 };
