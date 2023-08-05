8d57f32100bd3c50eb002475f3f30bd56bc41079
Kim Eunjeong <ryomi.j78@gmail.com>
2023-08-06 01:16:32 +0900

    add title edit function

--- module/editTitle.js
+++ module/editTitle.js
@@ -1,28 +1,46 @@
-import { getDOM } from '../utils/dom.js';
+import { TITLE_API_URL } from "../constants/constants.js";
+import { getDOM } from "../utils/dom.js";
 
-const $label = getDOM('.title');
-const $titleInput = getDOM('.titleInput');
-const value = $label.innerText;
+const $label = getDOM(".title");
+const $titleInput = getDOM(".titleInput");
+let titleValue = $titleInput.innerText
 
-export const editTitle = (e) => {
-  $label.style.display = 'none';
-  $titleInput.style.display = 'block';
-  $titleInput.focus();
-  $titleInput.value = '';
-  $titleInput.value = value;
+fetch(TITLE_API_URL)
+  .then(response => response.json())
+  .then(data => {
+    $label.innerText = data.title;
+    $titleInput.value = data.title;
+    titleValue = data.title
+  })
+  .catch(error => console.error('Error:', error));
+
+  export const editTitle = () => {
+  $label.style.display = "none";
+  $titleInput.style.display = "block";
 };
 
-export const confirmEditTitle = (e) => {
+export const updateTitle = async (e) => {
   if (e.keyCode === 27) {
-    $label.style.display = 'block';
-    $titleInput.style.display = 'none';
-    $titleInput.value = value;
-  }
-
-  if (e.keyCode === 13) {
-    $label.style.display = 'block';
-    $titleInput.style.display = 'none';
+    $label.style.display = "block";
+    $titleInput.style.display = "none";
+    $titleInput.value = titleValue;
+  } else if (e.keyCode === 13) {
     const newValue = $titleInput.value;
-    $label.innerText = newValue;
+    const res = await fetch(TITLE_API_URL, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: newValue }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    } else {
+      const data = await res.json();
+      $label.style.display = "block";
+      $titleInput.style.display = "none";
+      $label.innerText = newValue;
+
+      return data.title
+    }
   }
 };
