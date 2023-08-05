518920f93dd529afbfcc55b9482c6a2a041b60d1
Kim Eunjeong <ryomi.j78@gmail.com>
2023-08-05 23:16:50 +0900

    fix page error - only show neccessary page numbers

--- module/pagination.js
+++ module/pagination.js
@@ -1,23 +1,22 @@
 import { getAllDOM, getDOM } from '../utils/dom.js';
-import { getList } from './getList.js';
+import { getList, getListLength } from './getList.js';
 
 export let currentPage = 1;
-const totalCount = 53;
-const pageCount = 5;
 export const limit = 10;
 
-const $pagination = getDOM('.pagination');
-
-export const pagination = () => {
+export const pagination = async () => {
+  const totalCount = await getListLength()
+  const pageCount = totalCount && totalCount > 5 ? 5 : totalCount;
+  const $pagination = getDOM('.pagination');
   let totalPage = Math.ceil(totalCount / limit);
   let pageGroup = Math.ceil(currentPage / pageCount);
-
+  
   let lastNumber = pageGroup * pageCount;
   if (lastNumber > totalPage) {
     lastNumber = totalPage;
   }
-
-  let firstNumber = lastNumber - (pageCount - 1); 
+  
+  let firstNumber = lastNumber - (pageCount - 1) > 0 ? lastNumber - (pageCount - 1) : 1
 
   const next = lastNumber + 1; 
   const prev = firstNumber - 1;
@@ -39,6 +38,7 @@ export const pagination = () => {
   $pagination.innerHTML = html;
 
   const $currentPageNumbers = getAllDOM(`.pagination button`);
+
   $currentPageNumbers.forEach((button) => {
     button.addEventListener('click', () => {
       if (button.dataset.fn === 'prev') {
